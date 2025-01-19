41ca02a489a7bdfd85c43b4ac8c2cee50faaecb1
robinzclark777 <robinzclark@gmail.com>
2025-01-19 10:03:33 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+import pygame
+
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
